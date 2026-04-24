996ce02d07fb860138b2cc04dd1cf74aafde73f2
ElnazTayebi <elnazt67@gmail.com>
2026-04-24 11:11:38 +0200

    feat(state): add calculator statemanagement

--- src/compponents/CalculateForm.tsx
+++ src/compponents/CalculateForm.tsx
@@ -1,60 +1,71 @@
+import { useState } from "react";
+
 const CalculateForm = () => {
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-emerald-100">
-            
-            {/* Card */}
-            <div className="bg-white p-6 rounded-xl shadow-lg w-full max-w-sm">
 
-                {/* Title */}
-                <h1 className="text-lg font-bold text-emerald-700 mb-4 text-center">
-                    Calculator
-                </h1>
+    const [currentValue, setCurrentValue] = useState("0");
+    const [previousValue, setPreviousValue] = useState<string | null>(null);
+    const [operator, setOperator] = useState<string | null>(null);
+    const [memoryValue, setMemoryValue] = useState(0);
+    const [decimalPercision, setDecimalPercision] = useState(2);
 
-                {/* Display */}
-                <div className="bg-gray-100 text-right text-2xl font-semibold p-4 rounded-lg mb-4 overflow-hidden truncate">
-                    123.45
-                </div>
+    
 
-                {/* Decimal Range */}
-                <div className="mb-4">
-                    <label className="block text-sm text-gray-600 mb-1">
-                        Decimal Places
-                    </label>
+        return (
+            <div className="flex items-center justify-center min-h-screen bg-emerald-100">
 
-                    <input
-                        type="range"
-                        min="0"
-                        max="8"
-                        defaultValue="2"
-                        className="w-full"
-                    />
+                {/* Card */}
+                <div className="bg-white p-6 rounded-xl shadow-lg w-full max-w-sm">
 
-                    <div className="text-sm text-gray-500 mt-1 text-center">
-                        2 digits
+                    {/* Title */}
+                    <h1 className="text-lg font-bold text-emerald-700 mb-4 text-center">
+                        Calculator
+                    </h1>
+
+                    {/* Display */}
+                    <div className="bg-gray-100 text-right text-2xl font-semibold p-4 rounded-lg mb-4 overflow-hidden truncate">
+                        123.45
                     </div>
-                </div>
 
-                {/* Buttons */}
-                <div className="grid grid-cols-4 gap-3">
+                    {/* Decimal Range */}
+                    <div className="mb-4">
+                        <label className="block text-sm text-gray-600 mb-1">
+                            Decimal Places
+                        </label>
 
-                    {/* Numbers */}
-                    {["7", "8", "9", "/",
-                      "4", "5", "6", "*",
-                      "1", "2", "3", "-",
-                      "0", ".", "=", "+"].map((btn) => (
-                        <div
-                            key={btn}
-                            className="bg-emerald-100 text-emerald-700 hover:bg-emerald-500 hover:text-white transition-colors p-4 rounded-lg text-center cursor-pointer"
-                        >
-                            {btn}
+                        <input
+                            type="range"
+                            min="0"
+                            max="8"
+                            defaultValue="2"
+                            className="w-full"
+                        />
+
+                        <div className="text-sm text-gray-500 mt-1 text-center">
+                            2 digits
                         </div>
-                    ))}
+                    </div>
+
+                    {/* Buttons */}
+                    <div className="grid grid-cols-4 gap-3">
+
+                        {/* Numbers */}
+                        {["7", "8", "9", "/",
+                            "4", "5", "6", "*",
+                            "1", "2", "3", "-",
+                            "0", ".", "=", "+"].map((btn) => (
+                                <div
+                                    key={btn}
+                                    className="bg-emerald-100 text-emerald-700 hover:bg-emerald-500 hover:text-white transition-colors p-4 rounded-lg text-center cursor-pointer"
+                                >
+                                    {btn}
+                                </div>
+                            ))}
+
+                    </div>
 
                 </div>
-
             </div>
-        </div>
-    );
-};
+        );
+    };
 
-export default CalculateForm;
+    export default CalculateForm;
